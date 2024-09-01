da31fa32b13e7c58be462bfa420e8c6106e3aa76
ﾑ丂んズﾑ刀 <108182589+oxidvip@users.noreply.github.com>
2024-09-01 16:58:29 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 585
-        versionName = "1.9.2"
+        versionName = "1.9.1"
         multiDexEnabled = true
         splits {
             abi {
